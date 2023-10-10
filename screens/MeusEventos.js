1517d295f3a72f07ab1a4041fe59dd2127f6523f
Kauan Victor <Kauan.victor@iesb.edu.br>
2023-10-09 22:39:55 -0300

    Criar Segmentação Criados/Confirmados close #18

--- screens/MeusEventos.js
+++ screens/MeusEventos.js
@@ -1,8 +1,10 @@
 import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
+const Tab = createMaterialTopTabNavigator();
 
-function MeusEventos() {
+function ConfirmadosScreen() {
   const eventos = [
     {
       title: 'Mil ao Milhão',
@@ -55,6 +57,37 @@ function MeusEventos() {
     </View>
   );
 }
+
+function CriadosScreen() {
+  const eventos = [
+    {
+      title: 'Mil ao Milhão',
+      date: '23/09',
+      time: '19hrs',
+      description:
+        'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...',
+      imageSource: require('../assets/palestra1.jpg'),
+    },
+  ];
+
+  return (
+    <View style={{ flex: 1 }}>
+      <ScrollView>
+        {eventos.map((evento, index) => (
+          <EventCard
+            key={index}
+            title={evento.title}
+            date={evento.date}
+            time={evento.time}
+            description={evento.description}
+            imageSource={evento.imageSource}
+          />
+        ))}
+      </ScrollView>
+    </View>
+  );
+}
+
 const EventCard = ({ title, date, time, description, imageSource }) => (
   <View style={styles.card}>
     <Image source={imageSource} style={styles.image} />
@@ -70,7 +103,27 @@ const EventCard = ({ title, date, time, description, imageSource }) => (
   </View>
 );
 
-export default MeusEventos;
+export default function App() {
+  return (
+    <NavigationContainer independent={true}>
+      <Tab.Navigator
+        tabBarOptions={{
+          activeTintColor: 'white',
+          inactiveTintColor: 'white',
+          style: {
+            backgroundColor: 'purple', // Define a cor de fundo das abas como purple
+          },
+          indicatorStyle: {
+            backgroundColor: 'white', // Define a cor da linha indicadora como white
+          },
+        }}>
+        <Tab.Screen name="Confirmados" component={ConfirmadosScreen} />
+        <Tab.Screen name="Criados" component={CriadosScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
 const styles = StyleSheet.create({
   card: {
     flexDirection: 'row',
