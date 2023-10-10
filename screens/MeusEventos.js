6c64f0ef9ddf1395ec7947e33761da5529cf2d39
Kauan__Victor <108499771+Kauan00Victor@users.noreply.github.com>
2023-10-09 22:46:42 -0300

    Revert "Criar Tela Ajustes close #23"

--- screens/MeusEventos.js
+++ screens/MeusEventos.js
@@ -1,10 +1,8 @@
 import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
-const Tab = createMaterialTopTabNavigator();
 
-function ConfirmadosScreen() {
+function MeusEventos() {
   const eventos = [
     {
       title: 'Mil ao Milhão',
@@ -57,37 +55,6 @@ function ConfirmadosScreen() {
     </View>
   );
 }
-
-function CriadosScreen() {
-  const eventos = [
-    {
-      title: 'Mil ao Milhão',
-      date: '23/09',
-      time: '19hrs',
-      description:
-        'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...',
-      imageSource: require('../assets/palestra1.jpg'),
-    },
-  ];
-
-  return (
-    <View style={{ flex: 1 }}>
-      <ScrollView>
-        {eventos.map((evento, index) => (
-          <EventCard
-            key={index}
-            title={evento.title}
-            date={evento.date}
-            time={evento.time}
-            description={evento.description}
-            imageSource={evento.imageSource}
-          />
-        ))}
-      </ScrollView>
-    </View>
-  );
-}
-
 const EventCard = ({ title, date, time, description, imageSource }) => (
   <View style={styles.card}>
     <Image source={imageSource} style={styles.image} />
@@ -103,27 +70,7 @@ const EventCard = ({ title, date, time, description, imageSource }) => (
   </View>
 );
 
-export default function App() {
-  return (
-    <NavigationContainer independent={true}>
-      <Tab.Navigator
-        tabBarOptions={{
-          activeTintColor: 'white',
-          inactiveTintColor: 'white',
-          style: {
-            backgroundColor: 'purple', // Define a cor de fundo das abas como purple
-          },
-          indicatorStyle: {
-            backgroundColor: 'white', // Define a cor da linha indicadora como white
-          },
-        }}>
-        <Tab.Screen name="Confirmados" component={ConfirmadosScreen} />
-        <Tab.Screen name="Criados" component={CriadosScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
-}
-
+export default MeusEventos;
 const styles = StyleSheet.create({
   card: {
     flexDirection: 'row',
